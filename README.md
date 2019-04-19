930406f42c7a28d2912cb56d035f4d22d424ff76
hquan212 <rhq66@tamu.edu>
2019-04-19 10:39:31 -0500

    First push, need to connect Mongo

---
+++ README.md
@@ -0,0 +1,25 @@
+# This is a work in progress.
+
+Make sure you have the required programs installed to run:
+> Python
+> Docker-compose
+
+How to run,
+cd into main directory (Where the Dockerfile is)
+then run:
+> sudo docker-compose up --build
+
+Then go to chrome and type: 
+http://0.0.0.0:4000/ 
+
+right now there is nothing on the website.
+
+This is activate the docerfile and make it run. 
+
+For the guide:
+https://medium.com/@riken.mehta/full-stack-tutorial-flask-react-docker-420da3543c91
+https://medium.com/@riken.mehta/full-stack-tutorial-flask-react-docker-ee316a46e876
+https://medium.com/@riken.mehta/full-stack-tutorial-3-flask-jwt-e759d2ee5727
+
+
+Also you may like to download Postmate to try the POST, GET fucntions
--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import logging
 app = Flask(__name__)
 
 
-MONGO_HOST = "mongodb://cluster0-shard-00-00-s1qny.mongodb.net"
-MONGO_PORT = 27017
-MONGO_DB = "BloodBank"
-MONGO_USER = "sshjuser"
-MONGO_PASS = "SSHJ678!"
-client = MongoClient(MONGO_HOST, MONGO_PORT)
-db = client[MONGO_DB]
-db.authenticate(MONGO_USER, MONGO_PASS)
+# MONGO_HOST = "mongodb://cluster0-shard-00-00-s1qny.mongodb.net"
+# MONGO_PORT = 27017
+# MONGO_DB = "BloodBank"
+# MONGO_USER = "sshjuser"
+# MONGO_PASS = "SSHJ678!"
+# client = MongoClient(MONGO_HOST, MONGO_PORT)
+# db = client[MONGO_DB]
+# db.authenticate(MONGO_USER, MONGO_PASS)
 
-# client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
 
 db = client.tododb
 
